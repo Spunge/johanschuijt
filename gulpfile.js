f3e388c89d2883b098b04f705078b76a7e008028
Johan Schuijt <johan@publicpixels.nl>
2020-09-08 17:55:29 +0200

    add some text about past, add general layout

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,14 @@ const reload_html = function() {
     .pipe(livereload());
 }
 
+const copy_images = function() {
+  return src('src/img/*.jpg')
+    .pipe(dest('dist/img'));
+}
+
 exports.default = () => {
   livereload.listen();
+  copy_images();
   watch('src/sass/**/*.scss', compile_sass);
   watch('*.html', reload_html);
 }
